aef0950a6853bc78395af389e54496afd1eef595
Tai Duy Nguyen <duytai.cse@gmail.com>
2020-07-23 21:39:54 +0800

    use variables

--- publisher.js
+++ publisher.js
@@ -19,19 +19,26 @@ module.exports = {
   },
   createJob({ action, payload, expiredAt, timeout, assignedTo }) {
     const mutation = `
-      mutation {
+      mutation($action: String!, $payload: String!, $expiredAt: DateTime!, $assignedTo: ID!, $timeout: Int!) {
         createTask(
           to: JOB,
           input: {
-            action: "${action}"
-            payload: "${payload}",
-            expiredAt: "${expiredAt.toISOString()}"
-            assignedTo: "${assignedTo}"
-            timeout: ${timeout} 
+            action: $action
+            payload: $payload
+            expiredAt: $expiredAt 
+            assignedTo: $assignedTo
+            timeout: $timeout 
           }
         )
       }
     `
-    return endPoint.request(mutation)
+    const variables = {
+      action,
+      payload: JSON.stringify(payload),
+      expiredAt: expiredAt.toISOString(),
+      timeout,
+      assignedTo
+    }
+    return endPoint.request(mutation, variables)
   }
 }
